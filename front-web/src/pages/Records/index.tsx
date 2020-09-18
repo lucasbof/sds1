46b9cacd1b66f4326e053f621c16567199705f3e
Lucas Figueiredo Bof <lucasfbof@gmail.com>
2020-09-18 17:13:24 -0300

    configurando front-web para produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters/index';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-lucasbof.herokuapp.com';
 
 const Records = () => {
 
