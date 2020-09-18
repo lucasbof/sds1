c5a543105593fc06eb42d3ae97ad9e49a24e5096
Lucas Figueiredo Bof <lucasfbof@gmail.com>
2020-09-18 11:03:12 -0300

    Adicionando rotas no React native

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { FontAwesome5 as Icon } from '@expo/vector-icons'
 import { Text, View, Image, StyleSheet, Alert } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../../components/Header';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+    const navegation = useNavigation();
+
     const handleOnPress = () => {
-        Alert.alert('vc clicou no btn!')
+        navegation.navigate('CreateRecord');
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 <Image
                     source={require('../../assets/gamer.png')}
